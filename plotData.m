9d24fce6ddd34be9adbe2037dea856c34d323555
Hritik Kumar Sharma <44857105+Hritix@users.noreply.github.com>
2018-11-08 09:37:07 +0530

    Plot Data Function

---
+++ plotData.m
@@ -0,0 +1,6 @@
+function plotData(x, y)
+figure;
+plot(x,y, 'rx','MarkerSize',10);%plot the data
+ylabel('Profit in $10,000s');%set the y axis-label
+xlabel('Population of City in 10,000s');%set the x-axis-label
+end
